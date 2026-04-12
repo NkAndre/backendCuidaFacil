7c241f0ea4f9f4a22d305cfcb1681485b42090c6
NkAndre <andre270708@gmail.com>
2026-04-12 12:53:27 -0300

    feat:login e cadastro com tokej jwt

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User; 
+use Illuminate\Support\Facades\Auth; 
+use Illuminate\Support\Facades\Hash; 
+
+class AuthController extends Controller
+{
+    public function register(Request $request) 
+    {
+        // Validação básica
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'altura' => 'nullable',
+            'peso' => 'nullable'
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']), // Hash::make é o padrão moderno do Laravel
+            'altura' => $data['altura'],
+            'peso' => $data['peso'],
+        ]);
+
+        return response()->json([
+            'message' => 'Cadastrado com sucesso!', 
+            'user' => $user
+        ], 201);
+    }
+
+   public function login(Request $request)
+{
+    $credentials = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required', 
+    ]);
+
+    if (Auth::attempt($credentials)) {
+        $user = Auth::user();
+
+        // 1. GERA O TOKEN (O "CRACHÁ VIP")
+        // O nome 'auth_token' pode ser qualquer um
+        $token = $user->createToken('auth_token')->plainTextToken;
+        
+        return response()->json([
+            'message' => 'Login realizado!',
+            'token' => $token, 
+            'user' => [
+                'nome' => $user->name,
+                'email' => $user->email
+            ]
+        ], 200);
+    }
+
+    return response()->json(['error' => 'E-mail ou senha inválidos.'], 401);
+}
+}
